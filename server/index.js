08202f133f2567acd31bd9ee66fcb0c0047e2269
RubesWorld <hi@rubesworld.live>
2021-06-16 19:36:55 -0700

    Complete

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const server = require("./api/server");
 
 const PORT = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static("client/build"));
+}
+
 server.get("/", (req, res) => {
-  //   res.send("Hello Talkdesk Team");
-  res.json({ message: "Hello Talkdesk Team!" });
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 server.listen(PORT, () => {
